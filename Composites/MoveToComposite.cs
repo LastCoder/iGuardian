82047080ea6b7076f73a511ef2887f9de35f28ad
iuser99 <pro.kopis@aol.com>
2012-12-01 22:35:25 -0800

    Another reference error fix....
    Facepalmin'

--- Composites/MoveToComposite.cs
+++ Composites/MoveToComposite.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using iCombat.Wrappers;
+using iGuardian.Wrappers;
 using Typhon.BehaviourTree;
 using Typhon.Common;
 using Typhon.CommonBot.Navigation;
